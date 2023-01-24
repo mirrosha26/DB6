c510cbc956eae18fd21b209650828b6b523e7ec3
Mikhail Miroshnikov <106276818+mirrosha26@users.noreply.github.com>
2023-01-24 22:48:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,64 @@
-from models import Publisher, Book, Shop, Sale, Stock, create_tables
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from models import Publisher, Book, Shop, Stock, Sale, create_tables
+import json
+import sqlalchemy
 import os
 
-class Connect:
+def connect():
     DB_TYPE = os.getenv('DB_TYPE') or 'postgresql'
-    DB_NAME = os.getenv('DB_NAME') or 'postgres'
+    DB_NAME = os.getenv('DB_NAME') or 'test2'
     DB_USER = os.getenv('DB_USER') or 'postgres'
     DB_PASSWORD = os.getenv('DB_PASSWORD') or 'smog1718'
     DB_HOST = os.getenv('DB_HOST') or 'localhost'
     DB_PORT = os.getenv('DB_PORT') or 5432
     DSN = f'{DB_TYPE.lower()}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-    engine = create_engine(DSN)
-    Session = sessionmaker(bind=engine)()
+    return sqlalchemy.create_engine(DSN)
 
+def autofill_db_data(engine):
+    with open('tests_data.json', 'r') as f:
+        data = json.load(f)
+        Session = sessionmaker(engine)
+        session = Session()
+        for element in data:
+            if element['model'] == 'publisher':
+                instance = Publisher(name=element['fields']['name'])
+                session.add(instance)
+                session.commit()
+            elif element['model'] == 'book':
+                instance = Book(title=element['fields']['title'], id_publisher=element['fields']['id_publisher'])
+                session.add(instance)
+                session.commit()
+            elif element['model'] == 'shop':
+                instance = Shop(name=element['fields']['name'])
+                session.add(instance)
+                session.commit()
+            elif element['model'] == 'stock':
+                instance = Stock(id_book=element['fields']['id_book'], id_shop=element['fields']['id_shop'], count=element['fields']['count'])
+                session.add(instance)
+                session.commit()
+            elif element['model'] == 'sale':
+                instance = Sale(price=element['fields']['price'], date_sale=element['fields']['date_sale'], count=element['fields']['count'], id_stock=element['fields']['id_stock'])
+                session.add(instance)
+                session.commit()
+        session.close()
 
-def serching_publisher_name():
-    query_join = session.query(Sale).join(Stock).join(Book).join(Publisher)
-    query_publisher_name = input('Введите имя издателя: ')
-    query_result = query_join.filter(Publisher.publisher_name == query_publisher_name)
-    for result in query_result.all():
-        print(f'{result}')
-
-
-def serching_publisher_id():
-    publisher_id = input('Введите идентификатор издателя: ')
-    query_join = session.query(Sale).join(Stock).join(Book).join(Publisher)
-    
-    for row in query_join.filter(Book.id_publisher == publisher_id):
-        print(row)
-
-
+def info_book(request_in):
+    request_in = input('Введите имя или id издателя: ')
+    if request_in.isdigit():
+        for data in session.query(Publisher, Book, Stock, Shop, Sale).filter(Publisher.id == request_in).filter(Book.id_publisher == Publisher.id).filter(Stock.id_book == Book.id).filter(Shop.id == Stock.id_shop).filter(Sale.id_stock == Stock.id).all():
+            print(f'{data.Book.title} | {data.Shop.name} | {data.Sale.price * data.Sale.count} | {data.Sale.date_sale}')
+    else:
+        for data in session.query(Publisher, Book, Stock, Shop, Sale).filter(Publisher.name == request_in).filter(Book.id_publisher == Publisher.id).filter(Stock.id_book == Book.id).filter(Shop.id == Stock.id_shop).filter(Sale.id_stock == Stock.id).all():
+            print(f'{data.Book.title} | {data.Shop.name} | {data.Sale.price * data.Sale.count} | {data.Sale.date_sale}')
 
 
 if __name__ == '__main__':
-    session = Connect.Session
-    #serching_publisher_name()
-    serching_publisher_id()
+    engine = connect()
+
+    create_tables(engine)
+    #autofill_db_data(engine)
+
+    Session = sessionmaker(engine)
+    session = Session()
+    info_book(1)
     session.close()
